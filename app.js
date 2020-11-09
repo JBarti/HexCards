6b3808ac25de8c5e8829e7d5519a705c42a2c248
JBarti <josip.bartulovic3@skole.hr>
2020-11-09 21:03:45 +0100

    Fixes :hammer:

--- app.js
+++ app.js
@@ -5,29 +5,11 @@ const { generate_grid, fill_hexagon, grid_state, empty_hexagon } = require("./sc
 
 const DECK_API_URL = "https://hex-cards-api.herokuapp.com/decks";
 
-const DEFAULT_HEX_WIDTH = 200;
-const DEFAULT_HEX_HEIGHT = 174;
-const DEFAULT_GRID_AUTO_ROWS = 87;
-const DEFAULT_GRID_AUTO_COLUMNS = 151;
-
-
-function zoom_grid(scale) {
-	const hexes = document.querySelectorAll(".hex-container>.hex");
-	hexes.forEach((hex) => {
-		hex.style.width = `${DEFAULT_HEX_WIDTH * scale}px`;
-		hex.style.height = `${DEFAULT_HEX_HEIGHT * scale}px`;
-	});
-
-	const grid_container = document.querySelector(".hex-container");
-	grid_container.style.gridAutoRows = `${DEFAULT_GRID_AUTO_ROWS * scale}px`;
-	grid_container.style.gridAutoColumns = `${DEFAULT_GRID_AUTO_COLUMNS * scale}px`;
-}
-
 
 document.querySelector(".container-cards").addEventListener("cardSelect", (event) =>
 {
-	const image_url = event.detail.image_url;
-	fill_hexagon(image_url);
+    const image_url = event.detail.image_url;
+    fill_hexagon(image_url);
 
 });
 
@@ -35,40 +17,36 @@ generate_grid();
 tab_setup();
 
 request(DECK_API_URL, function(err, resp, body) {
-	const data = JSON.parse(body);
-	setup_decks(data);
+    const data = JSON.parse(body);
+    setup_decks(data);
 
 });
 grid_state.restore();
 window.addEventListener("hashchange", tab_setup);
 window.addEventListener("beforeunload", () => grid_state.store());
-document.querySelector(".scale-select")
-	.addEventListener("change", event => {
-		const scale_size = Number(event.target.value);
-		console.log(scale_size);
-		zoom_grid(scale_size);
-	});
 document.querySelector(".hex-delete")
-	.addEventListener("click", () => {
-		empty_hexagon();
-	});
+    .addEventListener("click", () => {
+        empty_hexagon();
+    });
 document.querySelector(".cards-tools>button")
-	.addEventListener("click", () => {
-		grid_state.clear();
-		location.reload();
-	});
+    .addEventListener("click", () => {
+        grid_state.clear();
+        location.reload();
+    });
 
 const click_colapse = click_colapse_sidebar_generator();
 document.querySelector(".btn-colapse")
-	.addEventListener(
-		"click",
-		click_colapse,
-	)
+    .addEventListener(
+        "click",
+        click_colapse,
+    );
 
 const btn_open = document.querySelector(".btn-open")
 btn_open
-	.addEventListener(
-		"click",
-		click_colapse,
-	);
+    .addEventListener(
+        "click",
+        click_colapse,
+    );
 btn_open.style.display = "none";
+
+const hex_container = document.querySelector(".hex-container");
--- scripts/grid.js
+++ scripts/grid.js
@@ -1,91 +1,91 @@
 const grid_state = {
-	data: {},
+    data: {},
 
-	add_card: function(card_pos, image_url) {
-		this.data[card_pos] = image_url;
-	},
+    add_card: function(card_pos, image_url) {
+        this.data[card_pos] = image_url;
+    },
 
-	remove_card: function(card_pos) {
-		delete this.data[card_pos]
-	},
+    remove_card: function(card_pos) {
+        delete this.data[card_pos]
+    },
 
-	store: function() {
-		window.localStorage.setItem("hex_grid_map", JSON.stringify(this.data));
-	},
+    store: function() {
+        window.localStorage.setItem("hex_grid_map", JSON.stringify(this.data));
+    },
 
-	restore: function() {
-		this.data = JSON.parse(window.localStorage.getItem("hex_grid_map") || "{}");
-		console.log(this.data);
-		Object.keys(this.data).forEach(hex_position => {
-			const hex = document.querySelector(`[data-position="${hex_position}"]`);
-			hex.style.backgroundImage = `url("${this.data[hex_position]}")`;
-		});
-	},
+    restore: function() {
+        this.data = JSON.parse(window.localStorage.getItem("hex_grid_map") || "{}");
+        console.log(this.data);
+        Object.keys(this.data).forEach(hex_position => {
+            const hex = document.querySelector(`[data-position="${hex_position}"]`);
+            hex.style.backgroundImage = `url("${this.data[hex_position]}")`;
+        });
+    },
 
-	clear: function() {
-		this.data = {};
-	}
+    clear: function() {
+        this.data = {};
+    }
 }
 
 
 function select_hexagon(event) {
-	const selected_hex = document.querySelector(".hex-container>.hex.bg-primary");
-	if(selected_hex) {
-		selected_hex.classList.remove("bg-primary");
-		selected_hex.classList.add("bg-light");
-		selected_hex.style.removeProperty("filter");
-	}
+    const selected_hex = document.querySelector(".hex-container>.hex.bg-primary");
+    if(selected_hex) {
+        selected_hex.classList.remove("bg-primary");
+        selected_hex.classList.add("bg-light");
+        selected_hex.style.removeProperty("border");
+    }
 
-	const new_select = event.target;
+    const new_select = event.target;
 
-	new_select.classList.add("bg-primary");
-	new_select.classList.remove("bg-light");
-	new_select.style.filter = "blur(2px)";
+    new_select.classList.add("bg-primary");
+    new_select.classList.remove("bg-light");
+    new_select.style.border =  "5px solid #007bff"
 }
 
 
 function fill_hexagon(image_url) {
-	const selected_hex = document.querySelector(".hex-container>.hex.bg-primary");
-	if(! selected_hex) {
-		return;
-	}
+    const selected_hex = document.querySelector(".hex-container>.hex.bg-primary");
+    if(! selected_hex) {
+        return;
+    }
 
-	selected_hex.style.backgroundImage = `url("${image_url}")`
-	grid_state.add_card(selected_hex.getAttribute("data-position"), image_url);
+    selected_hex.style.backgroundImage = `url("${image_url}")`
+    grid_state.add_card(selected_hex.getAttribute("data-position"), image_url);
 }
 
 
 function empty_hexagon() {
-	const selected_hex = document.querySelector(".hex-container>.hex.bg-primary");
-	if(! selected_hex) { 
-		return;
-	}
+    const selected_hex = document.querySelector(".hex-container>.hex.bg-primary");
+    if(! selected_hex) { 
+        return;
+    }
 
-	selected_hex.style.backgroundImage = "";
+    selected_hex.style.backgroundImage = "";
 
-	const hex_pos = selected_hex.getAttribute("data-position");
-	grid_state.remove_card(hex_pos);
+    const hex_pos = selected_hex.getAttribute("data-position");
+    grid_state.remove_card(hex_pos);
 
 }
 
 
 function generate_grid() {
-	const template_hexagon = document.querySelector("#hex-template");
-	const grid_container = document.querySelector(".hex-container");
+    const template_hexagon = document.querySelector("#hex-template");
+    const grid_container = document.querySelector(".hex-container");
 
-	for (let j = 1; j <= 12; j++) {
-		for (let i = 1; i <= 12; i++) {
-			const hexagon = template_hexagon.content.cloneNode(true).firstChild;
-			hexagon.setAttribute("data-position", `${j}-${i}`);
-			hexagon.addEventListener("click", select_hexagon);
-			hexagon.style.gridRowStart = j;
+    for (let j = 1; j <= 16; j++) {
+        for (let i = 1; i <= 10; i++) {
+            const hexagon = template_hexagon.content.cloneNode(true).firstChild;
+            hexagon.setAttribute("data-position", `${j}-${i}`);
+            hexagon.addEventListener("click", select_hexagon);
+            hexagon.style.gridRowStart = j;
 
-			if (j % 2 == 1) hexagon.style.gridRow = `${i * 2} / span 2`;
-			else hexagon.style.gridRow = `${i * 2 - 1} / span 2`;
+            if (j % 2 == 1) hexagon.style.gridRow = `${i * 2} / span 2`;
+            else hexagon.style.gridRow = `${i * 2 - 1} / span 2`;
 
-			grid_container.appendChild(hexagon);
-		}
-	}
+            grid_container.appendChild(hexagon);
+        }
+    }
 }
 
 
