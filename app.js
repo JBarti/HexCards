609cca96361606449f1de5afaca9555c67f1c49c
JBarti <josip.bartulovic3@skole.hr>
2020-11-10 15:31:07 +0100

    Add fullscreen popup

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const request = require("request");
 const { setup_decks, tab_setup, click_colapse_sidebar_generator } = require("./scripts/navbar");
 const { generate_grid, fill_hexagon, grid_state, empty_hexagon } = require("./scripts/grid");
 
-const DECK_API_URL = "https://hex-cards-api.herokuapp.com/decks";
-// const DECK_API_URL = "http://0.0.0.0:3000/decks";
+//const DECK_API_URL = "https://hex-cards-api.herokuapp.com/decks";
+const DECK_API_URL = "http://0.0.0.0:3000/decks";
 
+
+function detect_mobile() {
+    return navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+}
 
 document.querySelector(".container-cards").addEventListener("cardSelect", (event) =>
 {
@@ -21,6 +31,7 @@ request(DECK_API_URL, function(err, resp, body) {
     setup_decks(data);
 
 });
+
 grid_state.restore();
 window.addEventListener("hashchange", tab_setup);
 window.addEventListener("beforeunload", () => grid_state.store());
@@ -57,3 +68,15 @@ document.querySelector(".zoom-scale")
             hex_container.style.transform = `scale(${e.target.value})`;
         }
     );
+
+
+if(detect_mobile()) {
+    document.querySelector(".grid-container").style.overflow = "auto";
+    $(".modal-fullscreen").modal("show");
+    document
+        .querySelector(".accept-fullscreen")
+        .addEventListener("click", () => {
+            document.documentElement.requestFullscreen();
+            $(".modal-fullscreen").modal("hide");
+        });
+}
