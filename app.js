08b262479b5fdbba00cda0835964e8e863d097e2
JBarti <jbartu00@fesb.hr>
2022-12-11 18:54:58 +0100

    Remove need for API

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-const request = require("request");
+const { decks } = require("./decks");
 
 const { setup_decks, tab_setup, click_colapse_sidebar_generator } = require("./scripts/navbar");
 const { generate_grid, fill_hexagon, grid_state, empty_hexagon } = require("./scripts/grid");
-
-const DECK_API_URL = "https://hex-cards-api.herokuapp.com/decks";
-//const DECK_API_URL = "http://0.0.0.0:3000/decks";
-
 
 function detect_mobile() {
     return navigator.userAgent.match(/Android/i)
@@ -17,77 +13,78 @@ function detect_mobile() {
         || navigator.userAgent.match(/Windows Phone/i)
 }
 
-document.querySelector(".container-cards").addEventListener("cardSelect", (event) =>
-{
-    const image_url = event.detail.image_url;
-    fill_hexagon(image_url);
-});
-
-generate_grid();
-tab_setup();
-
-request(DECK_API_URL, function(err, resp, body) {
-    const data = JSON.parse(body);
-    setup_decks(data);
-
-});
-
-window.location.hash = "#nav-decks";
-grid_state.restore();
-window.addEventListener("hashchange", tab_setup);
-window.addEventListener("beforeunload", () => grid_state.store());
-document.querySelector(".hex-delete")
-    .addEventListener("click", () => {
-        empty_hexagon();
-    });
-document.querySelector(".cards-tools>button")
-    .addEventListener("click", () => {
-        grid_state.clear();
-        location.reload();
-    });
-
-const click_colapse = click_colapse_sidebar_generator();
-document.querySelector(".btn-colapse")
-    .addEventListener(
-        "click",
-        click_colapse,
-    );
-
-const btn_open = document.querySelector(".btn-open");
-btn_open
-    .addEventListener(
-        "click",
-        click_colapse,
-    );
-btn_open.style.display = "none";
-
-const hex_container = document.querySelector(".hex-container");
-document.querySelector(".zoom-scale")
-    .addEventListener(
-        "change", 
-        function(e) {
-            hex_container.style.transform = `scale(${e.target.value})`;
-        }
-    );
-
-const showMessageStartup = localStorage.getItem("visited");
-if(!showMessageStartup || showMessageStartup == "false") {
-    $(".modal-tutorial").modal("show");
-}
-
-$(".modal-tutorial").on("hidden.bs.modal", function() {
-    const showMessage = document.querySelector("#showMessage").checked;
-    localStorage.setItem("visited", showMessage);
-});
-
-if(detect_mobile()) {
-    document.querySelector(".grid-container").style.overflow = "auto";
-    $(".modal-fullscreen").modal("show");
-    document
-        .querySelector(".accept-fullscreen")
-        .addEventListener("click", () => {
-            document.documentElement.requestFullscreen();
-            $(".modal-fullscreen").modal("hide");
+function init() {
+    document.querySelector(".container-cards").addEventListener("cardSelect", (event) =>
+    {
+            const image_url = event.detail.image_url;
+            fill_hexagon(image_url);
         });
+
+    generate_grid();
+    tab_setup();
+    setup_decks(decks);
+
+    window.location.hash = "#nav-decks";
+    grid_state.restore();
+    window.addEventListener("hashchange", tab_setup);
+    window.addEventListener("beforeunload", () => grid_state.store());
+    document.querySelector(".hex-delete")
+        .addEventListener("click", () => {
+            empty_hexagon();
+        });
+    document.querySelector(".cards-tools>button")
+        .addEventListener("click", () => {
+            grid_state.clear();
+            location.reload();
+        });
+
+    const click_colapse = click_colapse_sidebar_generator();
+    document.querySelector(".btn-colapse")
+        .addEventListener(
+            "click",
+            click_colapse,
+        );
+
+    const btn_open = document.querySelector(".btn-open");
+    btn_open
+        .addEventListener(
+            "click",
+            click_colapse,
+        );
+    btn_open.style.display = "none";
+
+    const hex_container = document.querySelector(".hex-container");
+    document.querySelector(".zoom-scale")
+        .addEventListener(
+            "change", 
+            function(e) {
+                hex_container.style.transform = `scale(${e.target.value})`;
+            }
+        );
+
+    $(".modal-tutorial").on("hidden.bs.modal", function() {
+        const showMessage = document.querySelector("#showMessage").checked;
+        localStorage.setItem("visited", showMessage);
+    });
+
+    if(detect_mobile()) {
+        document.querySelector(".grid-container").style.overflow = "auto";
+        $(".modal-fullscreen").modal("show");
+        document
+            .querySelector(".accept-fullscreen")
+            .addEventListener("click", () => {
+                document.documentElement.requestFullscreen();
+                $(".modal-fullscreen").modal("hide");
+            });
+    }
+
+    const showMessageStartup = localStorage.getItem("visited");
+    if(!showMessageStartup || showMessageStartup == "false") {
+        console.log("SHOW");
+        $(".modal-tutorial").modal("show");
+        console.log("FAIL");
+    }
+
 }
 
+window.onload = init;
